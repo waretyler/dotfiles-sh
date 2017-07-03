b238c6cc570ba89761c85d10d3565ffa750693b9
Tyler Ware <tyler.ware.777@gmail.com>
2017-07-03 17:06:17 -0600

    Adding more fancy fance aliases

--- aliases.sh
+++ aliases.sh
@@ -1,5 +1,3 @@
-alias -g e="nvim"
-alias -g ef='nvim `fzf -m`'
 alias -g .so=". ~/.zshrc"
 
 # LaTex
@@ -9,12 +7,20 @@ alias lt="tree -LC 2"
 alias less="less -r"
 
 alias patch.paste="pbpaste | patch -p0"
+
+alias ls.p='(cd $p && ls -1 -d **/.git | sed "s/\/.git//" )'
+alias cd.p='project_dir=$(ls.p | fzf) && cd "$p/$project_dir"' 
+
+alias -g e="nvim"
+alias -g ef='edit_files=$(fzf -m) && (echo $edit_files | xargs nvim -c "Denite buffer")'
+alias -g e.p='cd.p && ef'
+
 alias git.root="cd \$(git rev-parse --show-toplevel)"
 alias git.clean="(git ls-files -md | xargs git reset HEAD) && (git ls-files -md | xargs git checkout --) && (git ls-files -o --exclude-standard | xargs rm -rf)"
+alias git.hclone='(github_repo=$(select_github_repositories || exit 2) && cd $p && git clone "https://github.com/${github_repo}")'
+alias git.ppull='(cd.p && git pull)'
+
 alias s?="search"
-
-
-alias cd.p='cd $p/$(cd $p/ && (find -L . -mindepth 1 -maxdepth 2 -type d -print | cut -b3- | fzf))' 
 
 
 # macos specfic
--- init.sh
+++ init.sh
@@ -42,3 +42,10 @@ bindkey "\C-p" up-line-or-history
 #bind '"\C-f": forward-search-history'
 
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+
+# tabtab source for serverless package
+# uninstall by removing these lines or running `tabtab uninstall serverless`
+[[ -f /Users/tware/.nvm/versions/node/v7.10.0/lib/node_modules/serverless/node_modules/tabtab/.completions/serverless.zsh ]] && . /Users/tware/.nvm/versions/node/v7.10.0/lib/node_modules/serverless/node_modules/tabtab/.completions/serverless.zsh
+# tabtab source for sls package
+# uninstall by removing these lines or running `tabtab uninstall sls`
+[[ -f /Users/tware/.nvm/versions/node/v7.10.0/lib/node_modules/serverless/node_modules/tabtab/.completions/sls.zsh ]] && . /Users/tware/.nvm/versions/node/v7.10.0/lib/node_modules/serverless/node_modules/tabtab/.completions/sls.zsh
