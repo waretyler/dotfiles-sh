2f27aad08ff662f3ec486018ac81da118e80305d
Tyler Ware <tyler.ware.777@gmail.com>
2018-02-23 19:16:37 -0700

    adding perl & various aliases

--- aliases.sh
+++ aliases.sh
@@ -22,7 +22,7 @@ alias cleantex="rm *.{aux,fdb_latexmk,fls,log,pdf}"
 alias lt="tree -LC 2"
 alias less="less -r"
 
-alias patch.paste="(v; echo -e '\n') | patch -p0"
+alias patch.v="(v; echo -e '\n') | patch -p0"
 
 alias lf="ls_fzf"
 alias ls.d="(find . -type d | sed 's/\/$//')"
@@ -50,8 +50,9 @@ alias g.ppull='(psel | while read -r dir; do; cd $dir && git pull; done)'
 alias g.co='branch_to_checkout=$(git branch | fzf | cut -c 3-) && git checkout $branch_to_checkout'
 alias g.ls="git status | grep '^\\s*[a-zA-Z]*:' | awk '{print \$(NF)}'"
 alias g.fzf="g.ls | fzf -m --preview 'git diff HEAD {}'"
-alias gaf='git add `g.fzf`'
-
+alias g.next.ci="git rev-list --reverse --ancestry-path  HEAD..master | head -n 1"
+alias g.cif="git log --format=format:'%H %C(dim bold white)%an %Creset%s %C(white dim italic)%cr' \`echo \${git_fzf_format:-'--follow -- .'}\` | fzf -m --ansi --with-nth 2.. --preview 'git diff --color {1}~1 {1}' | awk '{print \$1}'"
+alias g.af='git add `g.fzf`'
 alias gh.select_repo='github_repo=$(select_github_repositories)'
 alias gh.clone='(gh.select_repo && cd $p && git clone "https://github.com/${github_repo}")'
 alias gh.view='(gh.select_repo && xdg-open "https://github.com/${github_repo}")'
@@ -75,7 +76,11 @@ alias g.bclean='git branch -D $(git branch | fzf -m)'
 alias g.spop='stash=$(git stash list | fzf --preview "git diff \$(echo {} | egrep -oh \"^[^:]*\")" | egrep -oh "^[^:]+") && git stash pop "$stash"'
 alias g.sub_check='for dir in $(ls); do; (cd $dir && echo $dir && git status); done'
 alias -g jq.cli="jq -cR '[splits(\" +\")]' | jq -s '.'"
+alias v.jq="v | jq"
 alias open="xdg-open"
 
 
 alias wreado="while read -r line; do;"
+
+
+alias tmux.ls="tmux list-sessions"
