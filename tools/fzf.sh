4f28c1e9ed2313397d86c0539da9e33a76235752
Tyler Ware <tyler.ware.777@gmail.com>
2018-08-06 14:29:41 -0600

    Adding UI control for selecting in default FZF

--- tools/fzf.sh
+++ tools/fzf.sh
@@ -7,3 +7,7 @@ source ~/.fzf.zsh
 if [ ! -z "$(which ag)" ]; then
   export FZF_DEFAULT_COMMAND='(ag --hidden --ignore node_modules --ignore .git --ignore .idea --ignore .DS_Store -f -g "") 2> /dev/null'
 fi
+
+
+export FZF_DEFAULT_OPTS='--bind="ctrl-alt-a:select-all+accept,alt-a:select-all,alt-u:deselect-all,alt-u:deselect-all+accept,alt-enter:print-query"'
+
